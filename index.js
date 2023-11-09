ab9fd3c1f70f1b67deac1afbf666df0d746a9c53
vanessa moultrie <imvanessa16@gmail.com>
2023-11-08 22:27:27 -0500

    more code changes to index.js

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const inquirer = require('inquirer');
 const LOGO  = require('./lib/logo');
 const { writeFile } = require('fs/promises');
 const { Triangle, Square, Circle } = require('./lib/shapes');
+
+let questions = [
+    {
+        name: "logoText",
+        type: "input",
+        message:
+          "Enter text for the logo. (Max 3 characters.)",
+        validate: (logoText) =>
+          logoText.length <= 3 ||
+          "The message must not contain more than 3 characters",
+      },
+
+]
